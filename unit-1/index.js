19232234d56904902414543d3e6bbab64536a193
Chinh Phan <chinhphan099@gmail.com>
2019-05-21 23:46:25 +0700

    search

--- unit-1/index.js
+++ unit-1/index.js
@@ -1,7 +1,10 @@
 var express = require('express'),
   app = express(),
-  port = 3000;
-
+  port = 3000,
+  users = [
+    {id: 1, name: 'Chinh'},
+    {id: 2, name: 'Yen'}
+  ];
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -13,11 +16,20 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
   res.render('users/index', {
-    users: [
-      {id: 1, name: 'Chinh'},
-      {id: 2, name: 'Yen'}
-    ]
+    users: users
   });
+});
+
+app.get('/users/search', function(req, res) {
+  var q = req.query.q;
+  var matchedUser = users.filter(function(user) {
+    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+
+  res.render('users/index', {
+    users: matchedUser,
+    q: q
+  })
 });
 
 app.listen(3000, function() {
