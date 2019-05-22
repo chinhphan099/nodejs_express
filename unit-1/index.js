2a51ca5c9840e838061dcd1027416b6ab37fbdd1
Chinh Phan <chinhphan099@gmail.com>
2019-05-22 22:36:24 +0700

    create user via post method auto refresh by nodemon package

--- unit-1/index.js
+++ unit-1/index.js
@@ -4,10 +4,15 @@ var express = require('express'),
   users = [
     {id: 1, name: 'Chinh'},
     {id: 2, name: 'Yen'}
-  ];
+  ],
+  bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+// bodyParser use for red.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
   res.render('index', {
@@ -32,6 +37,15 @@ app.get('/users/search', function(req, res) {
     q: q
   })
 });
+
+app.get('/users/create', function(req, res) {
+  res.render('users/create');
+});
+
+app.post('/users/create', function(req, res) {
+  users.push(req.body);
+  res.redirect('/users');
+})
 
 app.listen(3000, function() {
   console.log('Server listening on port ' + port);
