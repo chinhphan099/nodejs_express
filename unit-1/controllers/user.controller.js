355fba437532625f19b620c7b0318f4cd05e793d
Chinh Phan <chinhphan099@gmail.com>
2019-05-30 23:30:05 +0700

    Controller

---
+++ unit-1/controllers/user.controller.js
@@ -0,0 +1,47 @@
+var db = require('../db');
+var shortid = require('shortid');
+var user = {};
+
+user.index = function(req, res) {
+  res.render('users/index', {
+    users: db.get('users').value()
+  });
+};
+
+user.search = function(req, res) {
+  var q = req.query.q;
+  var matchedUser = db.get('users').value().filter(function(user) {
+    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+
+  res.render('users/index', {
+    users: matchedUser,
+    q: q
+  })
+};
+
+user.create = function(req, res) {
+  res.render('users/create');
+};
+
+user.postCreate = function(req, res) {
+  req.body.id = shortid.generate();
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
+};
+
+user.get = function(req, res) {
+  var id = req.params.id;
+  var user = db.get('users').find({id: id}).value();
+  res.render('users/view', {
+    user: user
+  });
+};
+
+user.remove = function(req, res) {
+  var id = req.params.id;
+  var user = db.get('users').remove({id: id}).write();
+  res.redirect('/users');
+};
+
+module.exports = user;
--- unit-1/routes/user.route.js
+++ unit-1/routes/user.route.js
@@ -1,53 +1,24 @@
 var express = require('express');
 var db = require('../db');
-var shortid = require('shortid');
 var router = express.Router();
+var userController = require('../controllers/user.controller');
 
 // Render all users
-router.get('/', function(req, res) {
-  res.render('users/index', {
-    users: db.get('users').value()
-  });
-});
+router.get('/', userController.index);
 
 // Get query string from form Search -> then Render by using index paget as template.
-router.get('/search', function(req, res) {
-  var q = req.query.q;
-  var matchedUser = db.get('users').value().filter(function(user) {
-    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-
-  res.render('users/index', {
-    users: matchedUser,
-    q: q
-  })
-});
+router.get('/search', userController.search);
 
 // Create user page
-router.get('/create', function(req, res) {
-  res.render('users/create');
-});
+router.get('/create', userController.create);
+
 // Post data from create form then write data to db and redirect to users page.
-router.post('/create', function(req, res) {
-  req.body.id = shortid.generate();
-  db.get('users').push(req.body).write();
-  res.redirect('/users');
-})
+router.post('/create', userController.postCreate);
 
 // Show user info by render from view page.
-router.get('/:id', function(req, res) {
-  var id = req.params.id;
-  var user = db.get('users').find({id: id}).value();
-  res.render('users/view', {
-    user: user
-  });
-});
+router.get('/:id', userController.get);
 
 // Remove user via id then redirect to user page.
-router.get('/remove/:id', function(req, res) {
-  var id = req.params.id;
-  var user = db.get('users').remove({id: id}).write();
-  res.redirect('/users');
-});
+router.get('/remove/:id', userController.remove);
 
 module.exports = router;
