cf85533cb948e1ffff53ce91c1de1b04814fc3bb
Chinh Phan <chinhphan099@gmail.com>
2019-05-30 22:24:24 +0700

    - module shortid to create random id - view user info - remove user

--- unit-1/index.js
+++ unit-1/index.js
@@ -5,6 +5,7 @@ var express = require('express'),
   low = require('lowdb'),
   FileSync = require('lowdb/adapters/FileSync'),
   adapters = new FileSync('db.json'),
+  shortid = require('shortid'),
   db = low(adapters);
 
 db.defaults({users: []})
@@ -17,18 +18,21 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Index page
 app.get('/', function(req, res) {
   res.render('index', {
     name: 'Chinh'
   });
 });
 
+// Render all users
 app.get('/users', function(req, res) {
   res.render('users/index', {
     users: db.get('users').value()
   });
 });
 
+// Get query string from form Search -> then Render by using index paget as template.
 app.get('/users/search', function(req, res) {
   var q = req.query.q;
   var matchedUser = db.get('users').value().filter(function(user) {
@@ -41,15 +45,34 @@ app.get('/users/search', function(req, res) {
   })
 });
 
+// Create user page
 app.get('/users/create', function(req, res) {
   res.render('users/create');
 });
-
+// Post data from create form then write data to db and redirect to users page.
 app.post('/users/create', function(req, res) {
+  req.body.id = shortid.generate();
   db.get('users').push(req.body).write();
   res.redirect('/users');
 })
 
-app.listen(3000, function() {
+// Show user info by render from view page.
+app.get('/users/:id', function(req, res) {
+  var id = req.params.id;
+  var user = db.get('users').find({id: id}).value();
+  res.render('users/view', {
+    user: user
+  });
+});
+
+// Remove user via id then redirect to user page.
+app.get('/users/remove/:id', function(req, res) {
+  var id = req.params.id;
+  var user = db.get('users').remove({id: id}).write();
+  res.redirect('/users');
+});
+
+// Run localhost
+app.listen(port, function() {
   console.log('Server listening on port ' + port);
 });
